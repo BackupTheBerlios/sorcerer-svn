2c4ff1cf0656ca40b656d46f90ae08453bb913c7
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2002-07-21 03:59:42 +0000

    bad idea to remove the line to make the /var/run/apache dir.

--- trunk/grimoire/apache/apache-ssl.sh
+++ trunk/grimoire/apache/apache-ssl.sh
@@ -2,9 +2,11 @@
 
 case  $1  in
          start)  echo      "$1ing Apache web server."
+                 mkdir  -p  /var/run/apache
                  apachectl  startssl
                  ;;
   restart|stop)  echo      "$ing Apache web server."
+                 mkdir  -p  /var/run/apache
                  apachectl  $1
                  ;;
              *)  echo      "Usage: $0 {start|stop|restart}"
