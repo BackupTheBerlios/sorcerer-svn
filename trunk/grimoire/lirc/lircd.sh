18d4410e5a4ae7b84de89eb81207fcbc47c47c1b
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2004-05-31 02:48:36 +0000

    bug fixes

--- trunk/grimoire/lirc/lircd.sh
+++ trunk/grimoire/lirc/lircd.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-# Edit and uncomment the appropriate lines
-
 MODULE=
 
+echo  "$1ing lircd."
+
 case $1 in
-    start)  echo      "$1ing lircd."
-            modprobe  $MODULE
-	    /usr/sbin/lircd
-            ;;
-              
-     stop)  echo      "$1ing lircd."
-            pkill ^lircd &> /dev/null
-            ;;
-
-  restart)  $0  stop  &&
-            $0  start
-            ;;
-
-        *)  echo  "Usage: $0 {start|stop|restart}"
-            ;;
+    start)  [  -n    "$MODULE"  ]  &&
+            modprobe  $MODULE;  /usr/sbin/lircd     ;;
+     stop)  pkill -15  "^lircd$"                    ;;
+  restart)  $0  stop;  sleep 1;  $0  start          ;;
+        *)  echo  "Usage: $0 {start|stop|restart}"  ;;
 esac
