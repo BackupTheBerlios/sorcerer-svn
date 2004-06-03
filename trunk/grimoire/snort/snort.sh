8afea773fb90175947f0293048f5ef29a7c47ab8
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2004-06-03 21:41:02 +0000

    big changes for snort spell

--- trunk/grimoire/snort/snort.sh
+++ trunk/grimoire/snort/snort.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
+echo  "$1ing Snort Intrusion Detection System."
+
 case $1 in
-     start|restart)  echo   "$1ing Snort, Intrusion Detection System."
-                     pkill  "^snort$"
-                     snort  -TD
-                     ;;
-              stop)  echo   "$1ing Snort."
-                     pkill  "^snort$"
-                     ;;
-                 *)  echo   "Usage: $0 {start|stop|restart}"
-                     ;;
+    start)  /usr/bin/snort  -D  ;;
+  restart)  $0  stop;  sleep  1;  $0  start  ;;
+     stop)  pkill  "^snort$"  ;;
+        *)  echo   "Usage: $0 {start|stop|restart}"  ;;
 esac
