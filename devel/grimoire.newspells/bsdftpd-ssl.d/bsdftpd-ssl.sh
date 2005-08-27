987f208902be38f9abb995a60ceb893815ea23eb
cityhunter <cityhunter@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2005-08-27 21:11:03 +0000

    init.d fixes

--- devel/grimoire.newspells/bsdftpd-ssl.d/bsdftpd-ssl.sh
+++ devel/grimoire.newspells/bsdftpd-ssl.d/bsdftpd-ssl.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-FTPD="ftpd"
+
+### BEGIN INIT INFO
+# Provides: $network
+# Required-Start: $network
+# Required-Stop:  $network
+# Default-Start: 3 4 5
+# Default-Stop:  0 1 2 6
+# Short-Description: run bsdftp-ssl ftp server
+# Description:  run bsdftp-ssl ftp server
+### END INIT INFO
+
 
 case $1 in
 	start|restart)  echo   "$1ing bsdftpd-ssl, ftp daemon."
