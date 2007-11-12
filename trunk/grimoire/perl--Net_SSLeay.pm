11bf7d2bf330e784f62db34e0d9d98b2278e9a2c
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2007-11-12 10:19:20 +0000

    Updates

--- trunk/grimoire/perl--Net_SSLeay.pm
+++ trunk/grimoire/perl--Net_SSLeay.pm
@@ -1,11 +1,11 @@
-   VERSION=( "1.30" "1.25" )
-  CATEGORY="perl security/encryption"
- ATTRIBUTE="perl-module new"
+   VERSION=( 1.30 1.25 )
+  CATEGORY='perl security/encryption'
+ ATTRIBUTE='perl-module new'
     SOURCE="F/FL/FLORA/Net_SSLeay.pm-$VERSION.tar.gz"
-       URL="http://www.cpan.org/authors/id/"
-  HOMEPAGE="http://search.cpan.org/search?dist=Net_SSLeay.pm"
-       REQ="openssl perl--ExtUtils-ParseXS"
-  ESTIMATE="140"
+       URL='http://www.cpan.org/authors/id/'
+  HOMEPAGE='http://search.cpan.org/search?dist=Net_SSLeay.pm'
+       REQ='openssl perl--ExtUtils-ParseXS'
+  ESTIMATE='140'
   EPROVIDE="Net_SSLeay"
       DESC="perl--Net_SSLeay.pm allows scripts to call the functions in the OpenSSL library
 Net::SSLeay.pm - a perl module for calling
