2b9a1b1707a541ef16acad3b55abc845d89aa7a9
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2007-12-17 15:27:11 +0000

    Spells with mostly static definitions can index faster if a current index files is provided with the spell in the grimoire, but it does make the grimoire tarball larger.

---
+++ trunk/grimoire/INDEX/perl--Net_SSLeay.pm
@@ -0,0 +1,15 @@
+safe	perl--Net_SSLeay.pm	1.30
+safe	perl--Net_SSLeay.pm	1.25
+est	perl--Net_SSLeay.pm	140
+all	perl--Net_SSLeay.pm
+av	perl--Net_SSLeay.pm	1.30
+sho	perl--Net_SSLeay.pm	perl--Net_SSLeay.pm allows scripts to call the functions in the OpenSSL library
+pro	Net_SSLeay	perl--Net_SSLeay.pm
+epro	Net_SSLeay	perl--Net_SSLeay.pm
+att	perl-module	perl--Net_SSLeay.pm
+att	new	perl--Net_SSLeay.pm
+cat	perl	perl--Net_SSLeay.pm
+cat	security/encryption	perl--Net_SSLeay.pm
+req	perl--Net_SSLeay.pm	openssl
+req	perl--Net_SSLeay.pm	perl--ExtUtils-ParseXS
+src	perl--Net_SSLeay.pm	perl--Net_SSLeay.pm/1.30/Net_SSLeay.pm-1.30.tar.gz
