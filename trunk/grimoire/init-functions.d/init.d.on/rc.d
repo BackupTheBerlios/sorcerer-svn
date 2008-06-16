35b49aabc45a76cb9878e00166f0beeaf94fcc9d
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2008-06-16 21:03:38 +0000

    Updates

---
+++ trunk/grimoire/init-functions.d/init.d.on/rc.d
@@ -0,0 +1,23 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides: rc.d
+# Required-Stop: $local_fs udev
+# Should-Stop: initramfs
+# Default-Stop: 0 6
+# Short-Description: rc.d updates symbolic links in /etc/rc.d
+### END INIT INFO
+
+# Copyright 2008 by Kyle Sallee, all rights reserved.
+# for use with Sorcerer only
+
+. /lib/lsb/init-functions
+
+NAME=/etc/rc.d
+
+check(){ find /etc/init.d -maxdepth 1 -type f -newer /etc/rc.d | grep -q .; }
+update(){ /lib/lsb/install_initd; }
+
+if   log_warning_msg "$NAME checking"; check
+then log_warning_msg "$NAME updating"; update
+     log_success_msg "$NAME updated"
+fi;  log_success_msg "$NAME checked"
