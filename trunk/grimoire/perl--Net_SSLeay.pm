0718604bd16a6ffb40f6e81560a3949f5d0baeb2
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2008-07-11 21:54:36 +0000

    Updates

--- trunk/grimoire/perl--Net_SSLeay.pm
+++ trunk/grimoire/perl--Net_SSLeay.pm
@@ -1,16 +1,16 @@
-  stable 1.30 1.25
-  CATEGORY='perl security/encryption'
- ATTRIBUTE='perl-module new'
-    SOURCE="F/FL/FLORA/Net_SSLeay.pm-$VERSION.tar.gz"
-       URL='http://www.cpan.org/authors/id/'
-  HOMEPAGE='http://search.cpan.org/search?dist=Net_SSLeay.pm'
-       REQ='openssl perl--ExtUtils-ParseXS'
-  ESTIMATE='140'
-  EPROVIDE="Net_SSLeay"
-      DESC="perl--Net_SSLeay.pm allows scripts to call the functions in the OpenSSL library
+    stable 1.30 1.25
+   require openssl perl--ExtUtils-ParseXS
+
+  category perl security/encryption
+ attribute perl-module new
+    source F/FL/FLORA/Net_SSLeay.pm-$VERSION.tar.gz
+       url http://www.cpan.org/authors/id/
+  homepage http://search.cpan.org/search?dist=Net_SSLeay.pm
+  estimate 100
+      desc 'perl--Net_SSLeay.pm allows scripts to call the functions in the OpenSSL library
 Net::SSLeay.pm - a perl module for calling
 Secure Sockets Layer (SSL) functions of the
 SSLeay library directly from your perl scripts.
 It is useful if you want to program robots that
 access secure web servers or if you want to
-build your own applications over SSL encrypted tunnels."
+build your own applications over SSL encrypted tunnels.'
