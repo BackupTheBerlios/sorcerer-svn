6f76ca28ddb1b394a7f855264beb43314460457f
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2008-07-21 03:24:27 +0000

    Updates

--- trunk/grimoire/perl--Net_SSLeay.pm
+++ trunk/grimoire/perl--Net_SSLeay.pm
@@ -1,16 +1,10 @@
+# perl--Net_SSLeay.pm was replaced by perl--Net-SSLeay
     stable 1.30 1.25
    require openssl perl--ExtUtils-ParseXS
-
   category perl security/encryption
- attribute perl-module new
+ attribute perl-module deprecated
     source F/FL/FLORA/Net_SSLeay.pm-$VERSION.tar.gz
        url http://www.cpan.org/authors/id/
   homepage http://search.cpan.org/search?dist=Net_SSLeay.pm
   estimate 100
-      desc 'perl--Net_SSLeay.pm allows scripts to call the functions in the OpenSSL library
-Net::SSLeay.pm - a perl module for calling
-Secure Sockets Layer (SSL) functions of the
-SSLeay library directly from your perl scripts.
-It is useful if you want to program robots that
-access secure web servers or if you want to
-build your own applications over SSL encrypted tunnels.'
+      desc 'perl--Net_SSLeay.pm was replaced by perl--Net-SSLeay'
