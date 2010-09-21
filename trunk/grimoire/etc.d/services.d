c5a910f7248bea09a40347a0d39c8371bcd9fa62
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2010-09-21 09:35:01 +0000

    bug fixes

---
+++ trunk/grimoire/etc.d/services.d
@@ -0,0 +1,24 @@
+LOC=/etc/services.d/local
+WHY=Network
+DES="port numbers and names associations for TCP UDP services
+
+/etc/services
+maps service names to TCP and UDP port numbers.
+
+When installed a spell called \"services\"
+or called \"services-iana-direct\"
+can provide a monthly updated,
+large, comprehensive list of mappings
+provided by IANA,
+the organization responsible for
+port number to name server assignments.
+The file installed by the spell is called
+/etc/services.d/IANA
+
+However, any other file in /etc/services.d/
+is also merged into /etc/services
+/etc/services is generated without the comments
+contained in the original files.
+Doing so conserves memory."
+
+MAN=services
