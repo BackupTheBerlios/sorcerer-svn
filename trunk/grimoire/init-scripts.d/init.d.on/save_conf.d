8a2faab46ae98bc7f6e16be4b05b69d57aebbbd4
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2011-06-13 09:26:54 +0000

    new method for kbd and saving conf.d

---
+++ trunk/grimoire/init-scripts.d/init.d.on/save_conf.d
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Required-Start: $local_fs
+# Default-Start: S
+# Short-Description: saves configuration files that were modified during sysinit
+### END INIT INFO
+
+[ "$1" == try-restart ]              && exit 0
+[ -d            /etc/init.d/conf.d ] || exit 0
+[ -d /media/root/etc/init.d/conf.d ] || exit 0
+
+. /lib/lsb/init-functions
+
+start(){
+ run(){
+  find /etc/init.d/conf.d/ -type f |
+  while read; do
+   if   [      $REPLY -nt /media/root/$REPLY ]
+   then cp -av $REPLY     /media/root/$REPLY
+   fi
+  done
+ }
+
+ if   log_warning_msg "/etc/init.d/conf.d checking"; run
+ then log_success_msg "/etc/init.d/conf.d checked"
+ else log_success_msg "/etc/init.d/conf.d failed"
+ fi
+}
+
+start
