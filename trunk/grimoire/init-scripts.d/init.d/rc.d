5dc49f043b1ad3f8fb57205df0921d8f6cdf9fe4
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2011-08-03 01:33:10 +0000

    Updates

---
+++ trunk/grimoire/init-scripts.d/init.d/rc.d
@@ -0,0 +1,33 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Default-Mode: 500
+# Required-Stop: $local_fs
+# Should-Stop: initramfs tmp
+# Default-Stop: 0 6
+# Short-Description: rc.d updates symbolic links in /etc/rc.d
+### END INIT INFO
+
+# Copyright 2008-2011 by Kyle Sallee, all rights reserved.
+# for use with Sorcerer only
+
+. /lib/lsb/init-functions
+
+only stop status
+deny control
+
+check(){ find /etc/init.d -maxdepth 1 -type f -cnewer /etc/rc.d | grep -q . || ! [ -d /etc/rc.d ]; }
+update(){ /lib/lsb/install_initd; }
+
+status(){
+ if   log_warning_msg '/etc/rc.d checking'; check
+ then log_warning_msg '/etc/rc.d checked; status not current'; return 1
+ else log_success_msg '/etc/rc.d checked; status     current'
+ fi
+}
+
+stop(){
+ if ! status; then
+  log_warning_msg '/etc/rc.d updating'; update
+  log_success_msg '/etc/rc.d updated'
+ fi
+}
---
+++ trunk/grimoire/init-scripts.d/init.d/save_conf.d
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Default-Mode: 500
+# Required-Start: $local_fs
+# Default-Start: S
+# Short-Description: saves configuration files that were modified during sysinit
+### END INIT INFO
+
+[ -d            /etc/init.d/conf.d ] || exit 0
+[ -d /media/root/etc/init.d        ] || exit 0
+[ -d /media/root/etc/init.d/conf.d ] || mkdir -m 700 \
+     /media/root/etc/init.d/conf.d
+
+. /lib/lsb/init-functions
+
+only start
+deny control
+
+start(){
+ run(){
+  find /etc/init.d/conf.d/ -type f |
+  while read; do
+   if   [      $REPLY -nt /media/root/$REPLY ]
+   then cp -av $REPLY     /media/root/$REPLY
+   fi
+  done
+ }
+
+ if   log_warning_msg "/etc/init.d/conf.d checking"; run
+ then log_success_msg "/etc/init.d/conf.d checked"
+ else log_failure_msg "/etc/init.d/conf.d failed"
+ fi
+}
