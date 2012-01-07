ac4f9af25e33772e44aa81afa2def69d44aed84f
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2012-01-07 08:18:18 +0000

    bug fixes

--- trunk/grimoire/md5crypt.d/md5crypt.c
+++ trunk/grimoire/md5crypt.d/md5crypt.c
@@ -13,11 +13,12 @@
 
 int main(int argc, char *argv[])
 {
- char *line	= NULL;
  char salt[]	= "$1$NaCl$";
+ char *line	= NULL;
  size_t len	= 0;
 
- while ((getline(&line, &len, stdin)) != -1) {
+ while ((len=(getline(&line, &len, stdin))) != -1) {
+  line[len - 1] = 0;
   puts(crypt(line,salt));
   free(line);
   line=NULL;
