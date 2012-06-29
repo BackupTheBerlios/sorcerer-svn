dce5fd20a2a6f9363c09b675b3f53c01f39ece76
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2012-06-29 22:48:43 +0000

    audits

--- trunk/grimoire/perl--Net_SSLeay.pm
+++
@@ -1,9 +0,0 @@
-# perl--Net_SSLeay.pm was replaced by perl--Net-SSLeay
-
-   version stable 1.30 1.25
-   require openssl perl--ExtUtils-ParseXS
-  category perl security/encryption
- attribute perl-module deprecated
-    source http://www.cpan.org/authors/id/F/FL/FLORA/Net_SSLeay.pm-$VERSION.tar.gz
-      info home http://search.cpan.org/search?dist=Net_SSLeay.pm
-      desc 'perl--Net_SSLeay.pm was replaced by perl--Net-SSLeay'
