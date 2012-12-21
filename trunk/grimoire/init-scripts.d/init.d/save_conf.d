cb4be1a9edd41e132c6ae1e7dea36258cd2b0d44
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2012-12-21 17:08:11 +0000

    updates

--- trunk/grimoire/init-scripts.d/init.d/save_conf.d
+++ trunk/grimoire/init-scripts.d/init.d/save_conf.d
@@ -7,23 +7,25 @@
 # Short-Description: saves configuration files that were modified during sysinit
 ### END INIT INFO
 
-[ -d /media/root/etc/init.d        ] || exit 0
-[ -d /media/root/etc/init.d/conf.d ] || mkdir -m 700 \
-     /media/root/etc/init.d/conf.d
-
 . /lib/lsb/init-functions
 
 only start
 deny control
 
 start(){
+
  run(){
-  find /etc/init.d/conf.d/ -type f |
-  while read; do
-   if   [      $REPLY -nt /media/root/$REPLY ]
-   then cp -av $REPLY     /media/root/$REPLY
-   fi
+  local r=0
+  for   d in /media/root/*; do
+   [ -d "$d/etc/init.d"        ] || continue
+   [ -d "$d/etc/init.d/conf.d" ] || mkdir -m 700 "$d/etc/init.d/conf.d"
+   for  f in /etc/init.d/conf.d/*; do
+    if   [      "$f" -nt "$d/$f" ]
+    then cp -av "$f"     "$d/$f" || r=1
+    fi
+   done
   done
+  return $r
  }
 
  if   log_warning_msg "/etc/init.d/conf.d checking"; run
