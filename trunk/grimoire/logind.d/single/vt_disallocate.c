067a4da32dbf101a23e762b6fa2f351fcd54a127
cromwell <cromwell@a77899cc-cfd4-0310-a286-aa96c6c9c1cb>
2013-12-05 07:07:37 +0000

    bug fixes and cosmetic fixes

--- trunk/grimoire/logind.d/single/vt_disallocate.c
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/vt.h>
-
-int main ( int argc, char *argv[] ){
-ioctl ( open ( "/dev/console", O_RDWR ), VT_DISALLOCATE, 0 );
-}
